d00f238bc2c4ce9d02c3b57b99ca1ee41dc00767
Mikhail Zhuravlev <zhuravlevma@pik.ru>
2017-11-30 20:47:45 +0300

    Add project files.

---
+++ SaveAsTracker/SaveAsTrackerInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace SaveAsTracker
+{
+  public class SaveAsTrackerInfo : GH_AssemblyInfo
+  {
+    public override string Name
+    {
+      get
+      {
+        return "SaveAsTracker";
+      }
+    }
+    public override Bitmap Icon
+    {
+      get
+      {
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        return null;
+      }
+    }
+    public override string Description
+    {
+      get
+      {
+        //Return a short string describing the purpose of this GHA library.
+        return "";
+      }
+    }
+    public override Guid Id
+    {
+      get
+      {
+        return new Guid("f78a424d-925f-493d-8dd5-5c7832efd673");
+      }
+    }
+
+    public override string AuthorName
+    {
+      get
+      {
+        //Return a string identifying you or your company.
+        return "";
+      }
+    }
+    public override string AuthorContact
+    {
+      get
+      {
+        //Return a string representing your preferred contact details.
+        return "";
+      }
+    }
+  }
+}
